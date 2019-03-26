2d93c1503d66ec4a016ef131354ffe39c4ef9d1a
Kyle Hughes <blinded93@gmail.com>
2019-03-26 16:41:49 -0700

    devise config files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  # include Devise::Controllers::Helpers
+  acts_as_token_authentication_handler_for User, fallback: :none
+  acts_as_token_authentication_handler_for Admin, fallback: :none
+
+  private
+
+    def after_successful_token_authentication
+      renew_authentication_token!
+    end
 end
