3e0b68aba5e6db7d69121df5f820f489b13bbfcf
Kyle Hughes <blinded93@gmail.com>
2019-03-25 18:22:57 -0700

    add default json formats, admin routes and api sessions/user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     resources :items, only: [:index, :show]
     resources :materials, only: [:index, :show]
     resources :categories, only: [:index, :show]
+    resources :sessions, only: [:create, :destroy]
+    resources :users, only: [:create, :update, :destroy]
   end
 
-  namespace :admin do
+  namespace :admin, defaults: { format: :json } do
     resources :items, only: [:create, :update, :destroy]
-    resources :materials, only: [:update, :destroy]
-    resources :categories, only: [:update, :destroy]
+    resources :materials, only: [:create, :update, :destroy]
+    resources :categories, only: [:create, :update, :destroy]
+    resources :sessions, only: [:create, :destroy]
   end
 end
