2dda74e868079b804fe54130118178706e9550c1
Kyle Hughes <blinded93@gmail.com>
2019-03-26 18:39:20 -0700

    add models and associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   has_many :categories, through: :item_categories
   has_many :item_materials
   has_many :materials, through: :item_materials
+  
+  has_many :orders
+  has_many :order, through: :order_items
 end
