72e8d46de2f1b2ae267972a272f3d4de2909392c
Kyle Hughes <blinded93@gmail.com>
2019-04-05 18:45:45 -0700

    add Items container component

--- client/src/containers/Items.js
+++ client/src/containers/Items.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+import { CardDeck, Container } from 'react-bootstrap'
+import Item from '../components/Item'
+import { getItems } from '../actions/items'
+import Loading from '../components/Loading'
+import { connect } from 'react-redux'
 
-const Home = props => {
-  return (
-    <div>
-      <br /><br /><br /><br />This is only test.
-    </div>
-  )
+class Items extends Component {
+  componentWillMount() {
+    if (this.props.items.data.length <= 0) this.props.getItems()
+  }
+  
+  render() {
+    const { loading, data } = this.props.items
+
+    return (
+      <Fragment>
+        <Container className='mt-6'>
+          <CardDeck>
+            {
+              loading ? <Loading /> : data.map(item => {
+                return <Item key={item.id} {...item.attributes} />
+              })
+            }
+          </CardDeck>
+        </Container>
+      </Fragment>
+    )
+  }
 }
 
-export default Home
+const mapsStateToProps = state => ({ items: state.items })
+
+export default connect(mapsStateToProps, { getItems })(Items)
