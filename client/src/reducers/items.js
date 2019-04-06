e520f1891615a3270e6e00d4cf298115f46a57a7
Kyle Hughes <blinded93@gmail.com>
2019-04-05 18:47:15 -0700

    complete items reducer for GET_ITEMS and LOADING_ITEMS actions

--- client/src/reducers/items.js
+++ client/src/reducers/items.js
@@ -1,5 +1,14 @@
-export default function items(state = [], action) {
+export default function items(state = {
+  loading: false,
+  data: []
+}, action) {
   switch (action.type) {
+    case 'LOADING_ITEMS':
+      return { ...state, loading: true }
+
+    case 'GET_ITEMS':
+      return { loading: false, data: action.items.data }
+
     default:
       return state
   }
