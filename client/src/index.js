680553081a6094a7250c2317b8ced81200856cf3
Kyle Hughes <blinded93@gmail.com>
2019-04-05 18:43:14 -0700

    Began to implement Router Switch for Admin access

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './index.css';
-import App from './App';
-import reducers from './reducers/index'
+import App from './App'
+import Login from './containers/Login'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers/index'
 
-const store = createStore(reducers)
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Fragment>
+        <Switch>
+          <Route exact path='/admin' component={Login} />
+          <Route path='/' component={App} />
+        </Switch>
+      </Fragment>
+    </Router>
+    {/* <App /> */}
   </Provider>,
   document.getElementById('root')
 )
